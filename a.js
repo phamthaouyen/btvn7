d199511e1d54287daf3b7fe3646d1a7f0eca0438
phamuyen <94733070+phamthaouyen@users.noreply.github.com>
2022-03-06 16:06:36 +0700

    btvn(1) 7

---
+++ a.js
@@ -0,0 +1,9 @@
+var number = [[1, 2, 3],["a","b","c"]]
+for (var i=0 ; i< number.length; i++) {
+    console.log ( "    " +number [i][0] + "    ");
+    console.log ( "   " +number [i][0]+number [i][0]  +number [i][0] + "   ");
+    console.log ( "  " +number [i][0]+number [i][0]  +number [i][0]+number [i][0]  +number [i][0] + "   ");
+    console.log ( " " +number [i][0]+number [i][0]  +number [i][0]+number [i][0]  +number [i][0] +number [i][0]  +number [i][0] + "   ");
+    
+    
+}
